e8382a506fc941adc35f18f24de1d84f4d434ba4
wombat564 <lenca35@outlook.com>
2026-01-22 18:08:00 -0500

    Update products.js

--- products.js
+++ products.js
@@ -17,12 +17,31 @@ const productContainer = document.getElementById("products");
 
 products.forEach(product => {
   const div = document.createElement("div");
+
   div.innerHTML = `
-    <img src="${product.image}"
+    <img src="${product.image}" />
     <h3>${product.name}</h3>
     <p>$${product.price}</p>
-    <button>Add to Cart</button>
+    <button data-id="${product.id}">Buy Now</button>
   `;
+
+  const button = div.querySelector("button");
+
+  button.addEventListener("click", async () => {
+    const res = await fetch("/api/create-checkout-session", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        product: product
+      })
+    });
+
+    const data = await res.json();
+    window.location.href = data.url;
+  });
+
   productContainer.appendChild(div);
 });
+
+
 
